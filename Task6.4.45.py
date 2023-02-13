a3356159e19c9f0ecade89a20d3634712e16e1b6
Андрей Ковалев <kovalevandrey007@gmail.com>
2023-02-13 22:43:20 +0200

    SaveTask6.4.45

--- Task6.4.45.py
+++ Task6.4.45.py
@@ -1,4 +1,5 @@
-# №6.4[45] Два различных натуральных числа n и m называются дружественными, если сумма делителей числа n (включая 1, но исключая само n) равна числу m и наоборот.
+# №6.4[45] Два различных натуральных числа n и m называются дружественными,
+# если сумма делителей числа n (включая 1, но исключая само n) равна числу m и наоборот.
 # Например, 220 и 284 – дружественные числа.
 # 220: 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 и 110, — их сумма равна 284.
 # 284: 1, 2, 4, 71 и 142, — их сумма равна 220.
@@ -25,4 +26,36 @@
 #         2620 2924
 #         5020 5564
 #         6232 6368
+
+def sum_divisor(num: int) -> int:
+    """
+    :return sum of divisor for number num
+
+    :param num:int, sum number for .... calc sum
+    :return:int, sum of divisor
+    """
+    sum_a = 0
+    for i in range(1, a):
+        if a % i == 0:
+            sum_a +=i
+    return sum_a
+# навести на help(sum_divisor)
+def para_numbers(n: int) -> None:
+
+    for i in range(1, n): # i = 220
+        sum_1 = sum_divisor(i) # 284
+        sum_2 = sum_divisor(sum_1) # 220
+        if i == sum_2 and i < sum_1:
+            print(i, sum_1)
+
+para_numbers(10000)
+
+d = sorted([1,2,3])
+
+    # for i in range(1, n): # непошло
+    #     for j in range(1, n):
+    #         if i < j:
+    #             if friend_numbers(i, j): # i = 220
+    #                 print((i, j))
+
 
